116f3a602f1c67d77c123ecd09d717409dcfb276
OfficerPaulsn <78951659+OfficerPaulsn@users.noreply.github.com>
2021-02-12 08:48:58 +0100

    Update meiern.py

--- meiern.py
+++ meiern.py
@@ -1,1 +1,8 @@
-
+class wuerfel():
+  import random
+  
+  def erzeuge_wurf(anzahl_wuerfel):
+    wurf = list()
+    for wuerfel in range (anzahl_wuerfel):
+      wurf.append(random.randint(1,6))
+    return wurf
